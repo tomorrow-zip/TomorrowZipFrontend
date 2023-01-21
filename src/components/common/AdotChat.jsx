9bf79feda5861098c12a0fde9ce4f3553c0604e1
1lsang <ilsangpark.98@gmail.com>
2023-01-21 18:31:02 +0900

    feat: modify AdotChat components to align text center

--- src/components/common/AdotChat.jsx
+++ src/components/common/AdotChat.jsx
@@ -1,5 +1,9 @@
 const AdotChat = ({ children }) => {
-    return <h1 className="text-2xl font-bold mx-12 break-keep">{children}</h1>
+    return (
+        <h1 className="text-2xl font-bold mx-12 break-keep text-center">
+            {children}
+        </h1>
+    )
 }
 
 export default AdotChat
