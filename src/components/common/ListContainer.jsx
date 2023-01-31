aed7e862ec677ed0caa275e66e684ad37c14858e
1lsang <ilsangpark.98@gmail.com>
2023-01-31 11:34:56 +0900

    feat: add detail props for ListContainer component

--- src/components/common/ListContainer.jsx
+++ src/components/common/ListContainer.jsx
@@ -1,5 +1,33 @@
-const ListContainer = ({ children }) => {
-    return <div className="bg-list p-5 rounded-xl">{children}</div>
+const ChevronIcon = (props) => (
+    <svg
+        width={9}
+        height={14}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path
+            d="m1.375.75 6.25 6.696-6.25 5.804"
+            stroke="#000"
+            strokeWidth={1.5}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+)
+
+const ListContainer = ({ children, detail = false }) => {
+    return (
+        <div className="bg-list rounded-xl overflow-hidden">
+            <div className="px-5 py-4">{children}</div>
+            {detail && (
+                <button className="w-full border-t border-border px-5 py-3 text-center flex justify-center items-center gap-2.5">
+                    <span className="text-sm">자세히 살펴보기</span>
+                    <ChevronIcon />
+                </button>
+            )}
+        </div>
+    )
 }
 
 export default ListContainer
