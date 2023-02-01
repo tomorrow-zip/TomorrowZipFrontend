3007854d5aa72cca471f91c897217e42dd0dd24e
1lsang <ilsangpark.98@gmail.com>
2023-02-01 17:35:37 +0900

    feat: fix class AdotChat component to prevent select text

--- src/components/common/AdotChat.jsx
+++ src/components/common/AdotChat.jsx
@@ -1,6 +1,6 @@
 const AdotChat = ({ children }) => {
     return (
-        <h1 className="text-2xl font-bold mx-12 break-keep text-center">
+        <h1 className="text-2xl font-bold mx-12 break-keep text-center select-none">
             {children}
         </h1>
     )
