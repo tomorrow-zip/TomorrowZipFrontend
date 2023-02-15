0d214a3449b37517af7218c03e1105c52678e6f2
1lsang <ilsangpark.98@gmail.com>
2023-02-15 09:51:07 +0900

    feat: modify rendering for array of SelectFurnitureTypeButtons

--- src/components/home/SelectFurnitureTypeButtons.jsx
+++ src/components/home/SelectFurnitureTypeButtons.jsx
@@ -1,33 +1,48 @@
-import bed from "../../assets/icon/button/bed.png"
+import adotIcon from "../../assets/icon/button/furnitures/adot.png"
+import bedIcon from "../../assets/icon/button/furnitures/bed.png"
+import closetIcon from "../../assets/icon/button/furnitures/closet.png"
+import sofaIcon from "../../assets/icon/button/furnitures/sofa.png"
 import Button from "./Button.jsx"
 
-const SelectFurnitureTypeButtons = ({ next }) => {
+const furnitures = [
+    {
+        id: 0,
+        text: "에이닷이 추천해줘",
+        icon: adotIcon,
+    },
+    {
+        id: 1,
+        text: "침대",
+        icon: bedIcon,
+    },
+    {
+        id: 2,
+        text: "소파",
+        icon: sofaIcon,
+    },
+    {
+        id: 3,
+        text: "옷장",
+        icon: closetIcon,
+    },
+]
+const SelectFurnitureTypeButtons = ({ next, setType }) => {
     return (
         <div className="px-5 flex justify-start gap-2 overflow-x-scroll scrollbar-hide">
-            <Button
-                icon={bed}
-                onClick={() => {
-                    next()
-                }}
-            >
-                에이닷이 추천해줘
-            </Button>
-            <Button
-                icon={bed}
-                onClick={() => {
-                    next()
-                }}
-            >
-                침대
-            </Button>
-            <Button
-                icon={bed}
-                onClick={() => {
-                    next()
-                }}
-            >
-                책장
-            </Button>
+            {furnitures.map((furniture) => {
+                return (
+                    <Button
+                        icon={furniture.icon}
+                        onClick={() => {
+                            setType(furniture.id)
+                            next()
+                        }}
+                        key={furniture.text + furniture.id}
+                    >
+                        {furniture.text}
+                    </Button>
+                )
+            })}
         </div>
     )
 }
