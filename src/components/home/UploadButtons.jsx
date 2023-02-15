c4b416ec24812db46d68895368b85532c5c6669c
1lsang <ilsangpark.98@gmail.com>
2023-02-15 09:49:58 +0900

    fix: fix label for attribute for upload button

--- src/components/home/UploadButtons.jsx
+++ src/components/home/UploadButtons.jsx
@@ -1,4 +1,5 @@
-import bed from "../../assets/icon/button/bed.png"
+import albumIcon from "../../assets/icon/button/camera/album.png"
+import cameraIcon from "../../assets/icon/button/camera/camera.png"
 
 const UploadButtons = () => {
     return (
@@ -7,10 +8,9 @@ const UploadButtons = () => {
                 className="flex flex-row justify-between gap-2.5
                 bg-btn border border-border
                 rounded-full px-4 py-2.5 whitespace-nowrap cursor-pointer "
-                htmlFor="camera"
             >
                 <div className="w-6 h-6">
-                    <img src={bed} alt="icon" />
+                    <img src={cameraIcon} alt="icon" />
                 </div>
 
                 <div className="font-medium select-none">지금 사진 찍기</div>
@@ -27,19 +27,17 @@ const UploadButtons = () => {
                 className="flex flex-row justify-between gap-2.5
                 bg-btn border border-border
                 rounded-full px-4 py-2.5 whitespace-nowrap cursor-pointer"
-                htmlFor="camera"
             >
                 <div className="w-6 h-6">
-                    <img src={bed} alt="icon" />
+                    <img src={albumIcon} alt="icon" />
                 </div>
 
                 <div className="font-medium select-none">앨범에서 고르기</div>
 
                 <input
-                    id="camera"
+                    id="album"
                     type="file"
                     accept="image/png,image/jpeg,image/jpg"
-                    // capture="filesystem"
                     className="hidden"
                 />
             </label>
