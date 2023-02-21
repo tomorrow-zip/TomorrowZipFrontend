cbcd43ee6554f17e127504d538cac7900439ac1e
1lsang <ilsangpark.98@gmail.com>
2023-02-21 12:46:00 +0900

    feat: api proxy test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        proxy: {
+            "/test": {
+                target: "http://35.216.58.5/",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/test/, ""),
+            },
+        },
+    },
 })
