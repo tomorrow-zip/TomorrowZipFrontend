be733133259d3424bb56d2e057fe974a02dc0433
1lsang <ilsangpark.98@gmail.com>
2023-02-28 14:49:24 +0900

    fix: hotfix

--- src/components/layout/RecommendList.jsx
+++ src/components/layout/RecommendList.jsx
@@ -1,5 +1,6 @@
 import { useAtomValue } from "jotai"
 import { Link } from "react-router-dom"
+import dummyImg from "../../assets/img/269-0.jpeg"
 import ExampleImg from "../../assets/img/clock.png"
 import { analysisAtom } from "../../atoms/index.js"
 import { routes } from "../../router/router.jsx"
@@ -9,6 +10,27 @@ const RecommendList = () => {
     const analysis = useAtomValue(analysisAtom)
     return (
         <div className="flex flex-col gap-2 mx-4">
+            <ListContainer>
+                <Link to={`${routes.furniture}/269`}>
+                    <div className="flex justify-between items-center gap-2 pl-4 pr-3 py-3">
+                        <div>
+                            <h3 className="font-bold mb-1">
+                                이런 가구는 어때요?
+                            </h3>
+
+                            <div className="text-text-gray text-sm">
+                                <div>STRANDMON 스트란드몬</div>
+                                <div>₩ {(299900).toLocaleString()}</div>
+                            </div>
+                        </div>
+                        <img
+                            src={dummyImg}
+                            className="w-20 h-20 object-center object-cover rounded-lg bg-white"
+                            alt="example"
+                        />
+                    </div>
+                </Link>
+            </ListContainer>
             {analysis.recommend_list.map((item, idx) => {
                 return (
                     <ListContainer key={item.name + idx}>
