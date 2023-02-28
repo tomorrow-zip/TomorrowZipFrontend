2eeab6e3be23972f1578862cb882375962a7932c
1lsang <ilsangpark.98@gmail.com>
2023-02-28 12:50:00 +0900

    fix: fix image rotate issue

--- src/components/home/UploadButtons.jsx
+++ src/components/home/UploadButtons.jsx
@@ -1,3 +1,4 @@
+import loadImage from "blueimp-load-image"
 import { useSetAtom } from "jotai"
 import { useNavigate } from "react-router-dom"
 import { postImages } from "../../api/index.js"
@@ -28,11 +29,41 @@ const UploadButtons = () => {
 
     const onUpload = async (e) => {
         setLoading(true)
-        const img = e.target.files[0]
+        const file = e.target.files[0]
         // console.log(img)
         // alert(img)
         const formData = new FormData()
-        formData.append("file", img)
+        loadImage(
+            file,
+            function (img, data) {
+                // 2. 이미지 파일 데이터에 imageHead와 exif가 있는지 확인
+                if (data.imageHead && data.exif) {
+                    // 3. exif 값이 있다면 orientation 값을 1로 변경
+                    loadImage.writeExifData(
+                        data.imageHead,
+                        data,
+                        "Orientation",
+                        1
+                    )
+                    img.toBlob(function (blob) {
+                        loadImage.replaceHead(
+                            blob,
+                            data.imageHead,
+                            async function (newBlob) {
+                                newBlob.name = file.name
+                                // 4. 기존 메서드로 파일 s3에 업로드
+                                formData.append("file", newBlob)
+                            }
+                        )
+                    }, "image/jpeg")
+                } else {
+                    // exif 값 없으면 바로 s3에 업로드
+                    formData.append("file", file)
+                }
+            },
+            { meta: true, orientation: true, canvas: true }
+        )
+
         const response = await postImages(formData)
         console.log(response)
         // setUuid(response.data.result.imageUuid)
